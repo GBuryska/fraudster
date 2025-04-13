58e8b271d5ad825217047b9355fe7f4f5dbf1b9e
Alexander <agp92610@gmail.com>
2025-04-13 17:03:51 -0500

    updated

--- src/types.d.ts
+++ src/types.d.ts
@@ -17,6 +17,23 @@ export interface Transaction {
 
 export interface Settings {
     customer_id: string;
+    start_time?: string;
+    end_time?: string;
+    online_purchases?: boolean;
+    fraudscore_detection?: boolean;
+    daily_limit?: number;
+    weekly_limit?: number;
+    monthly_limit?: number;
+    purchase_limit?: number;
+    block_online_purchases?: boolean;
+    block_international_purchases?: boolean;
+    block_interstate_purchases?: boolean;
+    home_state?: string;
+    block_intercity_purchases?: boolean;
+    home_city?: string;
+    gambling?: boolean;
+    adult_entertainment?: boolean;
+    liquor_stores?: boolean;
 }
 
 export interface Users {
