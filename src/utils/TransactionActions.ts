415223404c86da215217588863caaf1272a66215
tamim <akmtamim.ahmed@go.winona.edu>
2025-04-16 09:16:43 -0500

    getPending

--- src/utils/TransactionActions.ts
+++ src/utils/TransactionActions.ts
@@ -44,3 +44,32 @@ export async function createTransaction(transaction: Transaction): Promise<Trans
         transaction
     );
 }
+
+// @ts-ignore
+export async function getPendingTransactions(userId: string): Promise<Transaction[]> {
+    const response = await databases.listDocuments(
+        '67e04d26003294165c25',
+        '67e04d2f0004159d8c8a',
+        [
+            Query.limit(20),
+            Query.equal('customer_id', userId),
+            Query.equal('transaction_status', 'pending')
+        ]
+    );
+
+    return response.documents.map((doc) => ({
+        $transaction_id: doc.$id,
+        customer_id: doc.customer_id,
+        card_number: doc.card_number,
+        transaction_timestamp: doc.transaction_timestamp,
+        transaction_amount: doc.transaction_amount,
+        transaction_currency: doc.transaction_currency,
+        transaction_type: doc.transaction_type,
+        transaction_status: doc.transaction_status,
+        merchant_id: doc.merchant_id,
+        merchant_name: doc.merchant_name,
+        transaction_location: doc.transaction_location,
+        fraud_score: doc.fraud_score,
+        online: doc.online
+    }));
+}
